606eab805704aa83d9058f5f48c5349554fb3e52
unknown <broken.wayan@gmail.com>
2022-04-11 17:14:20 +0800

    first init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#edebe8',
+        secondary: '#f6dccc',
+        tertiary: '#81d4fa',
+        dark: '#494245'
+      }
+    },
+  },
+  plugins: [],
+}
